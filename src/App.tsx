1a87aa7856e9b6bed7bed925eefd439be0394950
darmayasa221 <darmayasa221@gmail.com>
2023-02-28 12:24:11 +0800

    fix 04

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Global } from "@emotion/react";
 import Header from "./components/Header/Header";
 import { globalStyle } from "./globalStyle/globalStyle";
 import Main from "./components/Main/Main";
 
 function App() {
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(
-        `https://todo.api.devcode.gethired.id/activity-groups/38880`,
-      );
-      const responseJson = await response.json();
-      console.log(responseJson);
-    })();
-  }, []);
   return (
     <>
       <Global styles={globalStyle} />
